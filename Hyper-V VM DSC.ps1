fd62c7b9ed60ff22cb326a4e4580780abc1434b8
Zac B <bradyzp@gmail.com>
2016-01-30 11:45:02 +0100

    Added Working files
    Added Hyper-V VM DSC and PullServer-Configuration files - working on
    fleshing these out as part of our deployment goal.
    Currently working on automating the deployment of a DSC Pull Server and
    a single node that pulls from that server.

---
+++ Hyper-V VM DSC.ps1
@@ -0,0 +1,81 @@
+﻿Configuration VirtualMachine
+{
+    param (
+        [string]$HyperVNode = 'hawkwing.red-hawk.net',
+        [string]$BaseVHDPath = 'C:\Basevhd.vhdx',
+        [string]$DSCWebPath
+    )
+
+    Import-DscResource -Module xHyper-V,xPSDesiredStateConfiguration
+
+    $basedir = 'C:\Hyper-V\DSC\{0}'
+
+    $filecopy = @(
+                    @{'source'      = $basedir -f 'pullserver.mof';
+                      'destination' = 'Windows/System32/Configuration/pending.mof'},
+                    @{'source'      = $basedir -f 'unattend.xml';
+                      'destination' = 'unattend.xml'},
+                    @{'source'      = $DSCWebPath;
+                      'destination' = 'Program Files/WindowsPowerShell/Modules/xPSDesiredStateConfiguration'}
+                )
+
+    
+    Node $HyperVNode
+    {
+        $VMName = 'DEV-DSCPULL-SRV02'
+        $path   = 'C:\Hyper-V\DSC\'
+
+        $vhdpath = $path + "{0}.vhdx"
+
+        xVHD VHD
+        {
+            ParentPath           = $BaseVHDPath
+            Name                 = $VMName
+            Path                 = $path
+            Generation           = 'Vhdx'
+            #MaximumSizeBytes     = 20GB
+            Ensure               = 'Present'
+        }
+        cVHDFile FileCopy
+        {
+            PartitionNumber = 4  
+            VhdPath = $vhdpath -f $VMName
+            FileDirectory = $filecopy | % {
+                MSFT_xFileDirectory {
+                    SourcePath = $_.source
+                    DestinationPath = $_.destination
+                }
+            }
+            DependsOn = "[xVHD]VHD"
+        }
+
+ 
+        xVMHyperV VirtualMachine
+        {
+            Name                 = $VMName
+            VhDPath              = $vhdpath -f $VMName
+            SwitchName           = "Red-Hawk Production"
+            State                = 'Off'
+            StartupMemory        = 2048MB
+            MACAddress           = "00155D8A54A0" 
+            Generation           = 2
+            DependsOn            = '[cVHDFile]FileCopy'
+        }
+    }
+}
+
+function Get-DscResourceModulePath
+{
+    param(
+        [Parameter(Mandatory)]
+        [string] $DscResourceName)
+
+    $dscResource = Get-DscResource $DscResourceName
+    $dscResource.Module.ModuleBase
+}
+
+
+VirtualMachine -DSCWebPath (Get-DscResourceModulePath 'xDSCWebService')
+
+#"$ConfigDataLocation\VMData\unattend.xml";
+#$ConfigDataLocation = Split-Path $MyInvocation.MyCommand.Definition;
